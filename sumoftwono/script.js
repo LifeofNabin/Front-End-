936146368d3ade4736c2db78fa13b76c3d012bab
Nabin <dhamala.nabin1@gmail.com>
2024-09-28 14:32:49 +0900

    Dom

--- sumoftwono/script.js
+++
@@ -1,9 +0,0 @@
-const function populateDiv(){
-    const a=document.getElementById("first_number").value
-    const b=document.getElementById("Second_number").value
-    const element=document.getElementById("finalsum");
-
-    element.innerHTML=parseInt(a)+parseInt(b)
-
-
-}
