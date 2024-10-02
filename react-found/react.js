183250419a75622f744102440eaa2210bdc2daa2
Nabin <dhamala.nabin1@gmail.com>
2024-10-02 11:38:56 +0900

    add

---
+++ react-found/react.js
@@ -0,0 +1,7 @@
+function addUser() {
+  const name = document.getElementById("name").value;
+  const password = document.getElementById("password").value;
+  const Cred = document.getElementById("Cred");
+  console.log(name);
+  console.log(password);
+}
