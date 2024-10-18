d30ace50d361d3b6c19ea598a2c1361c40b795db
Nabin <dhamala.nabin1@gmail.com>
2024-10-19 01:14:32 +0900

    add

--- react-found/react.js
+++ react-found/react.js
@@ -2,7 +2,7 @@ function addUser() {
   const name = document.getElementById("name").value;
   const password = document.getElementById("password").value;
   const outerDiv = document.createElement("div");
-  outerDiv.innerHTML = name;
+  outerDiv.innerHTML = (name, password);
   document.getElementById("Cred").appendChild(outerDiv);
   
 }
